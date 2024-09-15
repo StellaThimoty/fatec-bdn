3a7ec002b4ac1370f6e839c8161366c69f5b49aa
gnu uguu <gnu@uguu.gnu>
2024-09-15 13:24:17 -0300

    bdn logo e qualifiers

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,13 @@
+import bdn from '/Liga_Baroes_da_Noite_Logo.png'
+
 export default function Home() {
   return (
     <>
-      pinto cu bosta mijo
+      <img src={bdn} alt="Logo Barões da Noite" />
+      <div className="flex justify-between">
+        <h1 className="flex text-3xl text-white">REGULAMENTO</h1>
+        <h1 className="flex text-3xl text-white">REGULAMENTO</h1>
+      </div>
     </>
   )
 }
