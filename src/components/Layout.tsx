6f9940a255dbd8c41c3d4a714ed4eba512b8ddad
gnu uguu <gnu@uguu.gnu>
2024-09-15 23:20:22 -0300

    uguuuuu

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,30 @@
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
 import Rugal from '/Rugal_Proibido_2003.mp4'
+import FS from '/fero_shueider.mp4'
+import JA from '/jaca_axuul.mp4'
+import KG from '/klasfer_guizin.mp4'
 import Footer from './Footer'
 
+const vidLink = (): string => {
+  const num = Math.floor(Math.random() * (100)) + 1
+  if (num >= 1 && num < 33) {
+    return FS
+  } else if (num >= 33 && num < 66) {
+    return JA
+  } else if (num >= 66 && num < 100) {
+    return KG
+  } else if (num == 100) {
+    return Rugal
+  }
+
+  return ''
+}
+
+
 export default function Layout() {
+  const video = vidLink()
+
   return (
     <>
       <main className='grow bg-backgroundPurple/75 h-[100%] min-h-screen text-white'>
@@ -13,7 +34,7 @@ export default function Layout() {
         </div>
       </main>
       <Footer/>
-      <video autoPlay loop muted playsInline src={Rugal} id='video'/>
+      <video autoPlay loop muted playsInline src={video} id='video'/>
     </>
   )
 }
