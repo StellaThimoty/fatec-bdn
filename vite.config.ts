700d69bab64d646e45cecac89ad6f8db5ef9fae5
gnu uguu <vovodoido1@gmail.com>
2024-09-19 17:32:31 -0300

    added ghpages plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import { ghPages } from 'vite-plugin-gh-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
   base: '/bdn-react-vite/',
   server: {
     port: 7500
