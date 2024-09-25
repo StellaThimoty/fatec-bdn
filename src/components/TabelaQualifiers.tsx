ad8922dc61759393296ce99e029977471e09076b
gnu uguu <vovodoido1@gmail.com>
2024-09-24 23:58:26 -0300

    business rules

--- src/components/TabelaQualifiers.tsx
+++ src/components/TabelaQualifiers.tsx
@@ -13,7 +13,7 @@ export default function TabelaQualifiers() {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {data.slice(0,8).map((val) => (
+        {data.slice(0,12).map((val) => (
           <TableRow key={val.username} className="text-center text-lg border-4">
             <TableCell className="bg-textPurple/70 size-[3rem] border-4 border-black/100 hover:bg-textPurple/50">{val.position}</TableCell>
             <TableCell className="text-left text-textPurple bg-white/70 border-4 border-black/100 hover:bg-white/50">{val.username}</TableCell>
@@ -23,11 +23,11 @@ export default function TabelaQualifiers() {
         <TableRow className="text-center text-lg border-4">
           <TableCell colSpan={3} className="text-center text-lg border-4 border-black/100 text-neutral-400">
             <div className="flex flex-row align-center text-center items-center justify-center">
-              <FaChevronUp/>&nbsp;PASSAM PARA A PRÓXIMA FASE&nbsp;<FaChevronUp/>
+              <FaChevronUp/>&nbsp;CLASSIFICAM PARA O CAMPEONATO&nbsp;<FaChevronUp/>
             </div>
           </TableCell>
         </TableRow>
-        {data.slice(8,data.length).map((val) => (
+        {data.slice(12,data.length).map((val) => (
           <TableRow key={val.username} className="text-center text-lg border-4 border-black/100">
             <TableCell className="bg-textPurple/70 size-[3rem] border-4 border-black/100 hover:bg-textPurple/50">{val.position}</TableCell>
             <TableCell className="text-left text-textPurple bg-white/70 border-4 border-black/100 hover:bg-white/50">{val.username}</TableCell>
