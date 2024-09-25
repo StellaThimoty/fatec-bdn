ad28cd7c600e72a63816b487be7042147245308e
gnu uguu <vovodoido1@gmail.com>
2024-09-24 23:56:20 -0300

    adapted to the new json style

--- src/components/TabelaFinal.tsx
+++ src/components/TabelaFinal.tsx
@@ -1,62 +1,6 @@
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./ui/table";
 import { FaChevronUp, FaChevronDown } from "react-icons/fa";
-const data = [{
-  position: 1,
-  username: 'gnu/uguu',
-  total_points: 69,
-  record: '9-0',
-  dif_pts: 3,
-  tb: 0},
-  {
-  position: 2,
-  username: 'gnu/uguu',
-  total_points: 69,
-  record: '9-0',
-  dif_pts: 3,
-  tb: 0},
-  {
-  position: 3,
-  username: 'gnu/uguu',
-  total_points: 69,
-  record: '9-0',
-  dif_pts: 3,
-  tb: 0},
-  {
-  position: 4,
-  username: 'gnu/uguu',
-  total_points: 69,
-  record: '9-0',
-  dif_pts: 3,
-  tb: 0},
-  {
-  position: 5,
-  username: 'gnu/uguu',
-  total_points: 69,
-  record: '9-0',
-  dif_pts: 3,
-  tb: 0},
-  {
-  position: 6,
-  username: 'gnu/uguu',
-  total_points: 69,
-  record: '9-0',
-  dif_pts: 3,
-  tb: 0},
-  {
-  position: 7,
-  username: 'gnu/uguu',
-  total_points: 69,
-  record: '9-0',
-  dif_pts: 3,
-  tb: 0},
-  {
-  position: 8,
-  username: 'gnu/uguu',
-  total_points: 69,
-  record: '9-0',
-  dif_pts: 3,
-  tb: 0}
-] //Dados de teste 
+import data from '@/assets/classificacao.json'
 
 export default function TabelaFinal() {
   return (
@@ -72,14 +16,14 @@ export default function TabelaFinal() {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {data.slice(0,4).map((val) => (
-          <TableRow key={val.username} className="text-center text-lg border-4 border-black">
-            <TableCell className="bg-textPurple/70 size-[3rem] border-4 border-black/100 hover:bg-textPurple/50">{val.position}</TableCell>
-            <TableCell className="text-left text-textPurple bg-white/70 border-4 border-black/100 hover:bg-white/50">{val.username}</TableCell>
-            <TableCell className="bg-textPurple/70 size-[3rem] border-4 border-black/100 hover:bg-textPurple/50">{val.total_points}</TableCell>
-            <TableCell className="text-textPurple bg-white/70 h-[3rem] w-[5rem] border-4 border-black/100 hover:bg-white/50">{val.record}</TableCell>
-            <TableCell className="bg-textPurple/70 h-[3rem] w-[7rem] border-4 border-black/100 hover:bg-textPurple/50">{val.dif_pts}</TableCell>
-            <TableCell className="text-textPurple bg-white/70 size-[3rem] border-4 border-black/100 hover:bg-white/50">{val.tb}</TableCell>
+        {data.slice(0,4).map((val, index) => (
+          <TableRow key={val.name} className="text-center text-lg border-4 border-black">
+            <TableCell className="bg-textPurple/70 size-[3rem] border-4 border-black/100 hover:bg-textPurple/50">{index}</TableCell>
+            <TableCell className="text-left text-textPurple bg-white/70 border-4 border-black/100 hover:bg-white/50">{val.name}</TableCell>
+            <TableCell className="bg-textPurple/70 size-[3rem] border-4 border-black/100 hover:bg-textPurple/50">{val.points}</TableCell>
+            <TableCell className="text-textPurple bg-white/70 h-[3rem] w-[5rem] border-4 border-black/100 hover:bg-white/50">{val.wins} - {val.losses}</TableCell>
+            <TableCell className="bg-textPurple/70 h-[3rem] w-[7rem] border-4 border-black/100 hover:bg-textPurple/50">{val.diff_pts}</TableCell>
+            <TableCell className="text-textPurple bg-white/70 size-[3rem] border-4 border-black/100 hover:bg-white/50">{val.tie_breaker}</TableCell>
           </TableRow>
         ))}
         <TableRow className="text-center text-lg border-4 border-black">
@@ -90,14 +34,14 @@ export default function TabelaFinal() {
             </div>
           </TableCell>
         </TableRow>
-        {data.slice(4,data.length).map((val) => (
-          <TableRow key={val.username} className="text-center text-lg border-4 border-black/100">
-            <TableCell className="bg-textPurple/70 size-[3rem] border-4 border-black/100 hover:bg-textPurple/50">{val.position}</TableCell>
-            <TableCell className="text-left text-textPurple bg-white/70 border-4 border-black/100 hover:bg-white/50">{val.username}</TableCell>
-            <TableCell className="bg-textPurple/70 size-[3rem] border-4 border-black/100 hover:bg-textPurple/50">{val.total_points}</TableCell>
-            <TableCell className="text-textPurple bg-white/70 h-[3rem] w-[5rem] border-4 border-black/100 hover:bg-white/50">{val.record}</TableCell>
-            <TableCell className="bg-textPurple/70 h-[3rem] w-[7rem] border-4 border-black/100 hover:bg-textPurple/50">{val.dif_pts}</TableCell>
-            <TableCell className="text-textPurple bg-white/70 size-[3rem] border-4 border-black/100 hover:bg-white/50">{val.tb}</TableCell>
+        {data.slice(4,data.length).map((val, index) => (
+          <TableRow key={val.name} className="text-center text-lg border-4 border-black/100">
+            <TableCell className="bg-textPurple/70 size-[3rem] border-4 border-black/100 hover:bg-textPurple/50">{index}</TableCell>
+            <TableCell className="text-left text-textPurple bg-white/70 border-4 border-black/100 hover:bg-white/50">{val.name}</TableCell>
+            <TableCell className="bg-textPurple/70 size-[3rem] border-4 border-black/100 hover:bg-textPurple/50">{val.points}</TableCell>
+            <TableCell className="text-textPurple bg-white/70 h-[3rem] w-[5rem] border-4 border-black/100 hover:bg-white/50">{val.wins} - {val.losses}</TableCell>
+            <TableCell className="bg-textPurple/70 h-[3rem] w-[7rem] border-4 border-black/100 hover:bg-textPurple/50">{val.diff_pts}</TableCell>
+            <TableCell className="text-textPurple bg-white/70 size-[3rem] border-4 border-black/100 hover:bg-white/50">{val.tie_breaker}</TableCell>
           </TableRow>
         ))}
       </TableBody>
