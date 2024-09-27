af3b3ab799aa36bafccc58495f75ffed7b59582f
vnvz <vvv@cesar.school>
2024-09-26 23:55:58 -0300

    alterar regulamento

--- src/routes/Regulamento.tsx
+++ src/routes/Regulamento.tsx
@@ -13,11 +13,15 @@ export default function Regulamento() {
           <Separator className='border-1'/>
           <AccordionContent className="pt-2 pl-3">
             <ul className="text-lg">
-              <li>- 16 jogadores (12 classificam ranking baseado em resultados em torneios, outros 4 entrarão por um open qualifier)</li>
-              <li>- Campeonato de pontos corridos (turno único)</li>
-              <li>- Partidas são FT3</li>
-              <li>- Top 8 se classifica para Fase 2</li>
-              <li>- 9o até 16o eliminados do torneio</li>
+              <li>- 16 jogadores compõem a liga:</li>
+              <ul className="pl-4">
+                <li>- 12 jogadores qualificados através da Classificação Pré-liga</li>
+                <li>- 4 jogadores qualificados através de um Open Qualifier</li>
+                </ul>
+              <li>- Campeonato de pontos corridos em turno único seguindo o Sistema de Pontuação Universal.</li>
+              <li>- Partidas são FT3.</li>
+              <li>- Top 8 se classifica para Fase 2.</li>
+              <li>- 9o até 16o eliminados do torneio.</li>
             </ul>
           </AccordionContent>
         </AccordionItem>
@@ -78,7 +82,7 @@ export default function Regulamento() {
                 <li>- 32 participantes ou menos: Top 8 pontua.</li>
                 <li>- Entre 33 participantes e 48: Top 12 pontua.</li>
                 <li>- Acima de 49 participantes: Top 16 pontua.</li>
-              </ul>  
+              </ul>
             </ul>
           </AccordionContent>
         </AccordionItem>
@@ -108,15 +112,36 @@ export default function Regulamento() {
               <li>Critérios de desempate:</li>
               <ul className="pl-4">
                 <li>- Pontos conquistados</li>
+                <li>- Recorde de partidas (W-L)</li>
                 <li>- Diferença de pontos conquistados e cedidos</li>
                 <li>- Tiebreaker (Número de partidas vencidas contra os adversários empatados)</li>
-                <li>- Recorde de partidas (W-L)</li>
                 <li>- FT2 entre os empatados, caso não seja possível desempatar através dos métodos acima.</li>
               </ul>
             </ul>
           </AccordionContent>
         </AccordionItem>
-        </Accordion>        
+        <AccordionItem value="item-5">
+          <AccordionTrigger className="text-2xl font-lemonmilk uppercase">Funcionamento das Rodadas</AccordionTrigger>
+          <Separator className='border-1'/>
+          <AccordionContent className="pt-2 pl-3">
+            <ul className="text-lg">
+              <li>- As rodadas estão separadas em 4 janelas de 3 dias de duração.</li>
+              <li>- As datas das janelas da Fase 1 estão dispostas da seguinte maneira, assim como a quantidade de rodadas a serem disputadas:</li>
+              <ul className="pl-4">
+                <li>- Semana 1: 11/10 até 13/10 / Rodadas 1, 2, 3 e 4</li>
+                <li>- Semana 2: 18/10 até 20/10 / Rodadas 5, 6, 7 e 8</li>
+                <li>- Semana 3: 25/10 até 27/10 / Rodadas 9, 10, 11 e 12</li>
+                <li>- Semana 4: 01/11 até 03/11 / Rodadas 13, 14 e 15</li>
+              </ul>
+              <li>- Em até 2 dias antes da abertura de uma nova janela de rodadas, serão selecionadas duas partidas por rodada para serem transmitidas ao-vivo no último dia de cada janela.</li>
+              <li>- Os jogadores que tiverem partidas off-stream, poderão marcar as partidas com seus oponentes em horários convenientes para ambas as partes.</li>
+              <li>- Os jogadores que estiverem na situação descrita no ponto acima também poderão transmitir ao-vivo as partidas, se assim preferirem.</li>
+              <li>- A rodada 15 será jogada obrigatoriamente no dia 03/11, com todos os jogos ao mesmo tempo, para assim garantir a idoneidade dos resultados da primeira fase.</li>
+              <li>- As datas das janelas da Fase 2 serão definidas após o encerramento da Fase 1.</li>
+            </ul>
+          </AccordionContent>
+        </AccordionItem>
+        </Accordion>
     </section>
     </div>
   )
