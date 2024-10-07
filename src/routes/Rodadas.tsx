8f9407e857feabaad63747db6186c0d07317c191
gnu uguu <vovodoido1@gmail.com>
2024-10-07 00:35:50 -0300

    Added some flags

--- src/routes/Rodadas.tsx
+++ src/routes/Rodadas.tsx
@@ -1,23 +1,31 @@
 import RodadaSelector from "@/components/RodadaSelector";
 import { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from "@/components/ui/accordion";
 export default function Rodadas() {
-  return (
+  const campComecou = false;
+  const fase1 = false;
+  const fase2 = false;
 
-    <Accordion type="single" collapsible className="w-full px-3 text-base">
-        <AccordionItem value="item-1">
+  return (
+    <>
+      {!campComecou ? <h1 className="text-4xl text-center mt-12 font-lemonmilk uppercase">Rodadas ainda não anunciadas!</h1> :
+      <Accordion type="single" collapsible className="w-full px-3 text-base">
+        {fase1 &&
+          <AccordionItem value="item-1">
           <AccordionTrigger className="text-2xl font-lemonmilk uppercase">Fase 1</AccordionTrigger>
           <hr className='border-1'/>
           <AccordionContent className="pt-2 pl-3">
             <RodadaSelector/>
           </AccordionContent>
-        </AccordionItem>
+        </AccordionItem>}
+        {fase2 &&
         <AccordionItem value="item-2">
           <AccordionTrigger className="text-2xl font-lemonmilk uppercase">Fase 2</AccordionTrigger>
           <hr className='border-1'/>
           <AccordionContent className="pt-2 pl-3">
             <RodadaSelector/>
           </AccordionContent>
-        </AccordionItem>
-    </Accordion>
+        </AccordionItem>}
+    </Accordion>}
+</>
   )
 }
