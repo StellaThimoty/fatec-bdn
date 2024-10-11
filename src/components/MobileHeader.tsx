187dd0fd55b551ce314cf8f471d484d344720cca
gnu uguu <vovodoido1@gmail.com>
2024-10-11 19:39:43 -0300

    Nex 404 image and fixed stuff

--- src/components/MobileHeader.tsx
+++ src/components/MobileHeader.tsx
@@ -2,6 +2,7 @@ import { NavLink } from 'react-router-dom'
 import { Sheet, SheetTrigger, SheetContent, SheetHeader, SheetTitle, SheetDescription } from './ui/sheet'
 import { Separator } from './ui/separator'
 import { GiHamburgerMenu } from 'react-icons/gi'
+import { FaMoon } from 'react-icons/fa'
 
 //  Everything in the middle could be put in a for loop since they all use the 
 // same classes and everything except for the first and last elements
@@ -13,22 +14,23 @@ export default function MobileHeader() {
         <SheetTrigger className='font-lemonmilk flex py-4 bg-navbarGray w-full text-textPurple'><GiHamburgerMenu className='mx-4 mt-0.5'/> LIGA BARÕES DA NOITE - UNIBR</SheetTrigger>
         <SheetContent side={'left'} className='bg-navbarGray font-lemonmilk w-[360px]'>
           <SheetHeader>
-            <SheetTitle className='text-lg uppercase'>Barões da Noite</SheetTitle>
+            <SheetTitle className='text-lg uppercase'>
+              <div className="flex flex-row justify-center align-center">Barões da Noite&nbsp;&nbsp;&nbsp;<FaMoon/></div></SheetTitle>
             <Separator className='bg-text-purple'/>
             <SheetDescription className='flex flex-col text-2xl grow-0'>
-            <NavLink to='home' className={({isActive}) => isActive ? "mx-5 py-4 text-textPurple decoration-4 underline-offset-4 underline" : "mx-5 my-4 text-textPurple underline-hover"}>
+            <NavLink to='home' className={({isActive}) => isActive ? "mx-5 py-4 text-textPurple decoration-4 underline-offset-4 underline" : "mx-5 my-4 text-textPurple"}>
             &nbsp;HOME&nbsp;
             </NavLink> 
-            <NavLink to='regulamento' className={({isActive}) => isActive ? "mx-5 py-4 text-textPurple decoration-4 underline-offset-4 underline" : "mx-5 my-4 text-textPurple underline-hover"}>
+            <NavLink to='regulamento' className={({isActive}) => isActive ? "mx-5 py-4 text-textPurple decoration-4 underline-offset-4 underline" : "mx-5 my-4 text-textPurple"}>
             &nbsp;REGULAMENTO&nbsp;
             </NavLink>
-            <NavLink to='rodadas' className={({isActive}) => isActive ? "mx-5 py-4 text-textPurple decoration-4 underline-offset-4 underline" : "mx-5 my-4 text-textPurple underline-hover"}>
+            <NavLink to='rodadas' className={({isActive}) => isActive ? "mx-5 py-4 text-textPurple decoration-4 underline-offset-4 underline" : "mx-5 my-4 text-textPurple"}>
             &nbsp;RODADAS&nbsp;
             </NavLink>
-            <NavLink to='tabela' className={({isActive}) => isActive ? "mx-5 py-4 text-textPurple decoration-4 underline-offset-4 underline" : "mx-5 my-4 text-textPurple underline-hover"}>
+            <NavLink to='tabela' className={({isActive}) => isActive ? "mx-5 py-4 text-textPurple decoration-4 underline-offset-4 underline" : "mx-5 my-4 text-textPurple"}>
             &nbsp;TABELA&nbsp;
             </NavLink>
-            <NavLink to='staff' className={({isActive}) => isActive ? "mx-5 py-4 text-textPurple decoration-4 underline-offset-4 underline" : "mx-5 my-4 text-textPurple underline-hover"}>
+            <NavLink to='staff' className={({isActive}) => isActive ? "mx-5 py-4 text-textPurple decoration-4 underline-offset-4 underline" : "mx-5 my-4 text-textPurple"}>
             &nbsp;STAFF&nbsp;
             </NavLink>
             </SheetDescription>
--- src/routes/Error.tsx
+++ src/routes/Error.tsx
@@ -1,11 +1,12 @@
 import { useRouteError } from 'react-router-dom'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import gif from '/suspense.gif'
+import mobile_bg from '/mobile_bg.png'
+import mika from '/mika_404.png'
 import { Suspense, lazy } from 'react'
 import MobileHeader from '../components/MobileHeader'
 import { Separator } from '@/components/ui/separator'
-import video from '/fuckedup_robot.mp4'
+import { cn } from '@/lib/utils'
 
 export default function ErrorPage() {
   type ErrorType = {
@@ -20,20 +21,23 @@ export default function ErrorPage() {
   console.error(error instanceof Error);
 
   return (
-      <div className="max-[723px]:bg-[url('/suspense.gif')] bg-cover">
+      <div className="max-[723px]:bg-[url('/mobile_bg.png')] bg-cover">
         <main className='grow bg-backgroundPurple/75 h-[100%] min-h-screen text-white'>
           {window.innerWidth >= 723 ? <Header/> : <MobileHeader/>}
-          <div className="p-6">
-            <h1 className="text-4xl font-lemonmilk uppercase">ERRO - {error.status} {error.statusText}</h1>
+          <div className="flex flex-col p-6 max-[723px]:items-center">
+            <h1 className="text-4xl font-lemonmilk uppercase">ERRO - {error.status}</h1>
+            <h2 className="text-3xl font-lemonmilk uppercase">{error.statusText}</h2>
             <Separator className='border-1 min-[723px]:w-[682px]'/>
-            <div className='flex justify-center mt-8 flex-col'>
-              <p>Acabou de acontecer um erro</p>
-              <p>{error.error.message}</p>
-              <video controls loop preload='auto' height={640} width={480} src={video}/>
+            <div className='mt-2 flex flex-col max-[723px]:items-center'>
+              <p className='ml-4 text-lg'>Acabou de acontecer um erro</p>
+              <p className='ml-4 text-lg'>{error.error.message}</p>
+              <img src={mika} alt='Mika segurando o Hyde 404' 
+              className={cn("aspect-square m-0", window.innerWidth >= 723 ? "size-1/3" : "")}/>
+              <p className="ml-4 text-lg">Art by: <a className='underline' href="http://twitter.com/veter0">@veter0</a>, Source: <a href="https://twitter.com/veter0/status/1171534802253598722" className="underline">here</a> </p>
               </div>
           </div>
-          <Suspense fallback={<img src={gif} id='bgVideo'></img>}>
-            {window.innerWidth >= 723 ? <Video/> : <img src={gif} id='bgVideo'></img> }
+          <Suspense fallback={<img src={mobile_bg} id='bgMobile'></img>}>
+            {window.innerWidth >= 723 ? <Video/> : <img src={mobile_bg} id='bgMobile'/> }
           </Suspense>
         </main>
         <Footer/>
